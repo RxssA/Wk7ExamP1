5ee61535dcf35fd2b519d067ab15bd033cc0e724
G00401655@atu.ie <G00401655@atu.ie>
2023-11-08 14:05:57 +0000

    First Main push

---
+++ src/main/java/ie/atu/week7exam/Week7ExamApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week7exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7ExamApplication.class, args);
+    }
+
+}
